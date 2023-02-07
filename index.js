13073d69dfa2e9ede33a92e6fe2dd33241dfb4f6
Saurabh <saur02052000abh@gmail.com>
2023-02-07 22:56:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function hambtogg() {
+    let x = document.getElementById("nav-content");
+     {
+         if(x.style.display ==="none")
+         {
+             x.style.display = "block";
+         }else {
+             x.style.display = "none"
+         }
+     }
+   }
